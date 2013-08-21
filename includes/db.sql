b7f029f76c190e88db60c6ff1a486952fe231ceb
fedex <fedex@fedex-geonode.(none)>
2013-08-21 17:24:54 -0300

    table structure update

--- includes/db.sql
+++ includes/db.sql
@@ -1,16 +1,12 @@
-- Table: app.maps
-
--- DROP TABLE app.maps;
 
 CREATE TABLE app.maps
 (
-  id serial NOT NULL,
-  portalconfig text,
-  tools text DEFAULT '{ "tools": [{ptype: "gxp_layertree", outputConfig: { id: "tree", border: true, tbar: []}, outputTarget: "westpanel"}, {ptype: "gxp_addlayers", actionTarget: "tree.tbar" }, { ptype: "gxp_removelayer", actionTarget: ["tree.tbar", "tree.contextMenu"]}, {ptype: "gxp_zoomtoextent",  actionTarget: "map.tbar"}, { ptype: "gxp_zoom", actionTarget: "map.tbar" }, { ptype: "gxp_navigationhistory", actionTarget: "map.tbar" },{ptype:"gxp_savemap",actionTarget:"map.tbar"}]}'::text,
-  sources text,
+  id integer NOT NULL DEFAULT nextval('app.maps_id_seq1'::regclass),
   map text,
   description text,
-  CONSTRAINT maps_pkey PRIMARY KEY (id )
+  created timestamp with time zone,
+  modified timestamp with time zone,
+  CONSTRAINT maps_pkey1 PRIMARY KEY (id )
 )
 WITH (
   OIDS=FALSE
